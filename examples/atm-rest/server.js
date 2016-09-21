265a6ba82aeb6eb8c4c3981956ee7f1b155bc378
k. corey  <kimler@gmail.com>
2016-09-20 22:44:46 -0400

    Adds image to front page

--- examples/atm-rest/server.js
+++ examples/atm-rest/server.js
@@ -14,13 +14,34 @@ app.use(parse.json());
 app.use(parse.json({type:'application/vnd.api+json'}));
 mongoose.connect('mongodb://127.0.0.1/atm-datas');
 
+app.use(express.static('public'));
 
 app.use('/api', require('./routes/api'));
 
 // Static Route
 app.get('/', function(req, res){
     res.send(`
+    <style>
+        .header img {
+        float: left;
+        width: 80px;
+        height: 80px;
+        background: #555;
+        opacity: 0.5;
+        }
+
+        .header h1 {
+        position: relative;
+        top: 18px;
+        margin-bottom: 50px;
+        left: 10px;
+        }
+    </style>
+    <div class="header">    
+    <img src="images/triped.jpg">
     <h1>Triped is running.</h1> 
+    </div>
+
     <p>get atm-data: <a href="http://`+ip+`:`+port+`/api/atm-datas">http://`+ip+`:`+port+`/api/atm-datas</a></p>
     <p></p>
     <p> get rules: <a href="http://`+ip+`:`+port+`/api/rules">http://`+ip+`:`+port+`/api/rules</a> </p>`);
